125bcd2fc78d8f897e2e74db49ce38ad30fb20de
Facundo Cersosimo <cersosimo.facundo@gmail.com>
2019-01-24 00:01:12 -0300

    solucionados problemas de diseño header y autocompletar

--- Componentes/header.php
+++ Componentes/header.php
@@ -9,11 +9,12 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light anchoTotal">
-	<a class="navbar-brand" href="#">CRUD</a>
+
 	<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 	<span class="navbar-toggler-icon"></span>
 	</button> -->
 	<div class="collapse navbar-collapse" id="navbarNavDropdown">
+	<a class="navbar-brand" href="#">CRUD</a>
 		<ul class="navbar-nav">
 			<li class="nav-item">
 				<a class="nav-link" href="../Vistas/alta.php">Alta</a>
